0125230231943e92d861cd7350dc87cc8799f659
Karina Shedlbauer <k@rina.wtf>
2019-10-10 17:43:54 +0300

    update start screen

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,21 +1,52 @@
 <template>
-  <v-card>
+  <v-card width="500px">
     <v-card-title
+      class="
+        display-1
+        d-flex
+        justify-center
+      "
       v-text="title"
     />
     <v-card-text
+      class="
+        subtitle-1
+        text-center
+        font-weight-light
+      "
       v-html="description"
     />
-    <div class="image-wrapper">
+    <p
+      class="
+        player-stats-headline
+        body-1
+        text-center
+        font-weight-light
+      "
+      v-text="'Основные показатели, по которым начисляем баллы:'"
+    />
+    <player-stats />
+    <div
+      class="
+        image-wrapper
+        d-flex
+        justify-center
+      "
+    >
       <v-img
         :src="image"
         lazy-src="/img/web_loader_dribble.gif"
-        aspect-ratio="1"
-        max-width="466"
+        max-width="476"
         max-height="300"
       />
     </div>
-    <v-card-actions>
+    <v-card-actions
+      class="
+        d-flex
+        flex-column
+        pa-3
+      "
+    >
       <v-btn
         block
         color="error"
@@ -28,10 +59,12 @@
 
 <script>
   import { mapState, mapActions } from 'vuex'
+  import PlayerStats from './PlayerStats.vue'
 
   export default {
     name: 'Start',
     components: {
+      PlayerStats,
     },
     computed: mapState({
       title: state => state.game.title,
@@ -41,9 +74,11 @@
     mounted () {
       this.getGameInfo()
     },
-    methods: mapActions([
-      'getGameInfo',
-      'startGame',
-    ]),
+    methods: {
+      ...mapActions([
+        'getGameInfo',
+        'startGame',
+      ]),
+    },
   }
 </script>
