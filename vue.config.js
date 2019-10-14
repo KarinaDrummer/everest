d5a69dc0de0d84fd5447ef794c814ac0dd42a35a
Karina Shedlbauer <k@rina.wtf>
2019-10-14 20:33:04 +0300

    update .env & move BASE_DIR from vue config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? process.env.PUBLIC_PATH
-    : process.env.DEV_PUBLIC_PATH,
-
   productionSourceMap: false,
 
   configureWebpack: {
