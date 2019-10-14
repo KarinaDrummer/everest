3a02fb975f48dec1c7a95a293dc36e4584acbd23
Karina Shedlbauer <k@rina.wtf>
2019-10-14 17:33:21 +0300

    add .env

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,11 @@ const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+    ? process.env.PUBLIC_PATH
+    : process.env.DEV_PUBLIC_PATH,
+
   productionSourceMap: false,
+
   configureWebpack: {
     plugins: [
       new CompressionPlugin(),
